0534fe716435deda2c3021971b9489544d66590b
irfanbasha <irfanbasha114@gmail>
2022-06-24 18:19:24 +0530

    small program

---
+++ Simple JS program/demo.js
@@ -0,0 +1,12 @@
+//divisable number 2
+var num1 = 1
+var num2 = 100
+console.log("number devisable by 3 is 1 to 100:")
+while(num1<=num2)
+{
+    if(num1%3==0) 
+    {
+        console.log(num1)
+    }
+    num1++
+}
